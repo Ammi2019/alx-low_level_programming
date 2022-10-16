bb7df7397ca14252ec247a922b488e26c1eaa834
Ammi2019 <mufwambiforb@gmail.com>
2022-10-16 12:32:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
+	/**
+	*The main function returns no arguments
+	*/
 	int main(void)
 {
-
-	puts ("\"programming is like building a multilingual puzzle");
-	
-	return (0);
+	/**
+	*Using the puts functio instead of prinf	
+	*/
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
 
 }
