759796a26856b89ef329188c2505ffceb7ec1a3e
Ammi2019 <mufwambiforb@gmail.com>
2023-02-22 02:59:40 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * main - print _putchar, followed by a new line
-* return 0 (success)
+* return:  0 (success)
 */
 
 int main(void)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_10(void);
 int _islower(int c);
@@ -12,6 +13,5 @@ void jack_bauer(void);
 void add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-int _putchar(char c);
 
 #endif		
