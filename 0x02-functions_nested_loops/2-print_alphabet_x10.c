28d578f9f5efbbe30db089954d9f51e35eb3b1ae
Ammi2019 <mufwambiforb@gmail.com>
2023-02-22 16:29:40 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
 
 void print_alphabet(void)
 {
-	char c;
+	char (c);
 	int i = 0;
 	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
-	}
-	_putchar(c);
 	{
+	_putchar(c);
+	}
 	_putchar('\n');
 	}
 	i++;
