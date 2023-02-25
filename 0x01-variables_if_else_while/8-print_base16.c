fae2397438078569d7fec1145bc5551aefbdbd95
Ammi2019 <mufwambiforb@gmail.com>
2023-02-25 14:03:58 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print number in base 16
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int r;
+	char low;
+
+	for (r = '0'; r <= '9'; r++)
+	putchar(r);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return(0);
+}
