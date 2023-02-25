a62e1e682cd7fd3cb3ca709409de3a7c2a89ff3f
Ammi2019 <mufwambiforb@gmail.com>
2023-02-25 11:35:17 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - print alphabet in lower case
+* Return: void
+*/
+
+int main(void)
+{
+
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++);
+
+	putchar(alph);
+
+	putchar('\n');
+
+	return (0);
+}
