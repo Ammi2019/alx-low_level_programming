73c4051886a3456bfaa258af7c07f9872d29a163
Ammi2019 <mufwambiforb@gmail.com>
2023-02-25 12:29:25 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,10 @@
 
 int main(void)
 {
+	char c;
 
-	char alph;
-
-	for (alph = 'a'; alph <= 'z'; alph++);
-	putchar(alph);
+	for (c = 'a'; c <= 'z'; c++);
+	putchar(c);
 	putchar('\n');
-
 	return (0);
 }
