d803d66e353c4ed7994c062994265399bda01bcf
Ammi2019 <mufwambiforb@gmail.com>
2023-02-26 10:21:07 -0800

    5-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,6 +4,7 @@
 /**
 * _abs - compute an absolute value of an integer
 * Return: c if less than 0 &&abs_val if absolute
+* c: of an in value
 */
 
 int _abs(int c)
@@ -12,8 +13,9 @@ int _abs(int c)
 	if (c < 0)
 	{
 	int abs_val;
+
 	abs_val = c * -1;
-	
+
 	return (abs_val);
 	}
 	return (c);
