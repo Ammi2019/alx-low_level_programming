acad405a42094a67933e556c6304b3978b8be34a
Ammi2019 <mufwambiforb@gmail.com>
2023-02-28 15:48:20 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,14 +11,14 @@ int _atoi(char *s)
 {
 
 	int c = 0;
-	unsigned int ni =0;
+	unsigned int ni = 0;
 	int min = 1;
 	int isi = 0;
 
 	while (s[c])
 	{
 	if (s[c] == 45)
-	{	
+	{
 	min *= -1;
 	}
 	while (s[c] >= 48 && s[c] <= 57)
@@ -34,7 +34,7 @@ int _atoi(char *s)
 	c++;
 	}
 	ni *= min;
-	return(ni);
+	return (ni);
 
 }
 
