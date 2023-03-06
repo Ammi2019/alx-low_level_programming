3785e762775c7a68875e73e4c6171cac746669ed
Ammi2019 <mufwambiforb@gmail.com>
2023-03-06 15:26:31 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locate character in string
+ * @s: source string
+ * @i:
+ * @c: character to find
+ * Return: the string from character found
+ */
+
+char *_strchr(char *s, char c)
+
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		return (s + i);
+	}		
+	return  (NULL);
+}
