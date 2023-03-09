9b7c615ed0f48352dd0d9852ec0b888a46001eb0
Ammi2019 <mufwambiforb@gmail.com>
2023-03-08 17:28:21 -0800

    0x08-recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pw_recursion - Returns the value of 10 raised to the power y
+ * @x: value to be multiplied
+ * @y: exponent value
+ * Return: the value multiplied by y
+ */
+
+int _pow_recursion(int x, int y)
+
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x* _pow_recursion(x, y- 1));
+}
