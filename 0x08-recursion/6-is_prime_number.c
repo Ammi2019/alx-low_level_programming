6d847b694de2143902f774ce5f56178e6ea59705
Ammi2019 <mufwambiforb@gmail.com>
2023-08-13 03:17:53 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,19 +3,20 @@
 /**
   * is_prime_number - a function to validate a prime number
   * @n: the prime number to be validated
-  * return: 1 if prime number; else return 0
+  * Return: 1 if prime number, else return 0
   */
 
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return(_is_actual_prime(n, n - 1));
+	return (_is_actual_prime(n, n - 1));
 }
 /**
   * _is_actual_prime - recursively calculates if a number is prime
   * @n: number to validate as prime
   * @a: the iterator
+  * Return: Prime number
   */
 
 
