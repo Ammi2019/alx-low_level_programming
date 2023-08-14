5fc687c3aab18bccffcae4d7886f5db5eaad75b1
Ammi2019 <mufwambiforb@gmail.com>
2023-08-14 22:19:33 +0200

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 /**
+  * @name: string to return the namet 
+  * @age: value to be printed in float type
+  * @owner: String to print owner
+  * Return: The Strings
   */
 
 struct dog
