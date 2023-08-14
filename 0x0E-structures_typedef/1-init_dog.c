2c6a542e2d493dbc55d26d1cc4097ef82522bf7f
Ammi2019 <mufwambiforb@gmail.com>
2023-08-15 00:22:25 +0200

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,13 +2,18 @@
 #include "dog.h"
 
 /**
+ * init_dog - function that initialize a variable of type struct dog
+ * @name: sting to input
+ * @age: string to input
+ * @owner: string to input
+ * Return: void
   */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if(d == NULL)
 	d = malloc(sizeof(struct dog));
-	
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
-  * @name: string to return the namet 
+  * @name: string to return the name
   * @age: value to be printed in float type
   * @owner: String to print owner
   * Return: The Strings
@@ -13,3 +14,16 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+  *dog_t - definition of type for dog
+  */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
