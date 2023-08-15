09714b373ec24a853da4d4f9710bec26971cb433
Ammi2019 <mufwambiforb@gmail.com>
2023-08-15 03:50:02 +0200

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,16 +7,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 	return ;
-	}
-	if (d->name == ("nil"))
-	{
-		return ;
-	}
-	if(d->owner == ("nil"))
-	{
-		return ;
-	}
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	
 	printf("%s\n%f\n%s\n", d->name, d->age, d->owner);
 }
