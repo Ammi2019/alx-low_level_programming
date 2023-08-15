9ab58ee3db598ff308244077efde8f68bb3560ce
Ammi2019 <mufwambiforb@gmail.com>
2023-08-15 03:09:51 +0200

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,6 @@ void print_dog(struct dog *d)
 	return ;
 	}
 		printf("%s\n", d->name ? d->name : "(nil)");
-		printf("%-8.1f\n", d->age);
+		printf("%f\n", d->age);
 		printf("%s\n", d->owner ? d->owner : "(nil)");
 }
