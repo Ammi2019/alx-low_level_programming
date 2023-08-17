0573aed90eb7605feed9ef8460895fae0e0cce5b
Ammi2019 <mufwambiforb@gmail.com>
2023-08-16 17:21:47 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,14 @@
 /** 
   * print_name -  function that prints a name
   * @name: The string to be printed
-  * @(*f): Function pointer
+  * @*f: Function pointer
+  * @*: pointer
   * Return: The String and or Void
   */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if(name == NULL && f == NULL)
+	if(name == NULL || f == NULL)
 		return;
 	f(name);
 }
