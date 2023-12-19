f6c656ae692cd1b7fd131806732c341daeeeb26f
Ammi2019 <mufwambiforb@gmail.com>
2023-12-19 21:21:54 +0200

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a function that prints the alphabet in lowercase
+ * 
+ * Return: alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+	_putchar('\n');
+	return;
+}
