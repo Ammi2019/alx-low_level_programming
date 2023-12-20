a383e81941e51f6632e5cc5d3041db6e189956e2
Ammi2019 <mufwambiforb@gmail.com>
2023-12-20 22:50:33 +0200

    2-alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a function to print the lowercase alphabet 10 times
+ *
+ * Return: 10 times the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	
+	int n = 0;
+	while (n <= 10)
+	{
+	for(c = 'a'; c <= 'z';) 
+	{
+	_putchar(c);
+	c++;
+	}
+	_putchar('\n');
+	n++;
+	}
+	return ;
+
+}
+
