b16e3883559da8c5146f4c7601b41135838089de
Ammi2019 <mufwambiforb@gmail.com>
2023-12-24 10:19:35 +0200

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase character
+ *
+ * Return: 1 if lower and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	for (c = 'a';c <= 'z'; c++)
+	{
+	return (1);
+	}
+	if ((c = 'A') && (c <= 'Z'))
+	{
+	return (0);
+	}
+	_putchar('\n');
+	return (0);
+
+}
