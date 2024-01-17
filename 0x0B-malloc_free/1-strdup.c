057e6c995eec723a7733b3ea8c1b4133b947fa1d
Ammi2019 <mufwambiforb@gmail.com>
2024-01-16 17:06:00 -0800

    0-create_array.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - a function that returns a pointer to a newly allocated
+ * space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: char
+ * Return: Always 0
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, b = 0;
+
+	if (str == NULL
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+	i++;
+
+	ptr = malloc(sizeof(char) * (i + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (b = 0; str[b]; b++)
+	ptr[b] = str[b];
+		return (ptr);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - a function that concatanates two strings
+ * @s1: stirng input one to concat
+ * @s2: string input two to concat
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = j = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	str = malloc(sizeof(char) * (i + j + 1));
+
+	if (str == NULL)
+		return (NULL);
+		
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++, j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
