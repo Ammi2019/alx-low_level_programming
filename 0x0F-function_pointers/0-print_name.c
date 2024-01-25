ac275efdd1f9426827577e6b0cd7eaa2f6a303e5
Ammi2019 <mufwambiforb@gmail.com>
2024-01-24 17:57:10 -0800

    Print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+* print_name - a function to a print name using pointer to function
+* @name: string
+* @f: pointer  var
+* Return: void
+**/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	return;
+	f(name);
+}
